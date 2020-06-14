5445de0eeb4a21444c85e386df423664ccd5c7e2
Jack Middlebrook <jack.middlebrook@gmail.com>
2020-06-14 14:00:41 +0800

    Add contact page

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,11 @@
+import BlogLayout from "src/layouts/BlogLayout/BlogLayout";
+
+const ContactPage = () => {
+  return (
+    <BlogLayout>
+      <div>test</div>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
